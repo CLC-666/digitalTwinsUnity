4d1938423c26d88408e3f8c845651e7bf5cdb070
Will Davis <W.Davis@mdx.ac.uk>
2020-01-15 13:13:34 +0000

    server works with unity - basic

--- PLC_TCP_Server_Data.py
+++ PLC_TCP_Server_Data.py
@@ -1,22 +1,47 @@
-import socket
-from time import sleep
-# One way control
+import socket, sys, time
 
-serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serv.bind(('192.168.1.86', 9999))
-serv.listen(5)
+socket.setdefaulttimeout(30000)
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Then bind() is used to associate the socket with the server address. In this case, the address is localhost, referring to the current server, and the port number is 10000.
+address = '10.3.121.133'
+port = 9999
+# Bind the socket to the port
+server_address = (address, port)
+print (sys.stderr, 'starting up on %s port %s' % server_address)
 
-testData = ["First induction sensor_SERVER", "RFID_SERVER", "End induction sensor_SERVER"]
-counter = 0
+sock.bind(server_address)
+# Calling listen() puts the socket into server mode, and accept() waits for an incoming connection.
+
+# Listen for incoming connections
+sock.listen(1)
 
 while True:
+    try:
+        # Wait for a connection
+        print (sys.stderr, 'waiting for a connection')
+        connection, client_address = sock.accept()
+        # accept() returns an open connection between the server and client, along with the address of the client. The connection is actually a different socket on another port (assigned by the kernel). Data is read from the connection with recv() and transmitted with sendall().
 
-    conn, addr = serv.accept()
-    from_client = ''
+        print (sys.stderr, 'connection from', client_address)
 
-    while True:
-        conn.send(testData[counter].encode())
+        # Receive the data in small chunks and retransmit it
+        while True:
+            try:
+                connection.sendall("hello2".encode())
+                time.sleep(1)
+                # data = connection.recv(10)
+                # if data:
+                #     print(data)
+                #     data = ''
+                #
+                #     # time.sleep(1)
+                # else:
+                #     print (sys.stderr, 'no more data from', client_address)
+                #     dataFollowing = 0
+                #     break
+            except KeyboardInterrupt:
+                break
 
 
-
-    conn.close()
+    except KeyboardInterrupt:
+        break
--- test.py
+++ test.py
@@ -2,6 +2,9 @@ import socket
 
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect(('192.168.1.86',9999))
+sock.connect(('10.3.121.133',9999))
 
 sock.sendall("hello".encode())
+while True:
+    print(sock.recv(10))
+    # print(sock.recv(1024).decode())
