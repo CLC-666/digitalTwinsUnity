eb09c5425ea75a1901c01d02c89e3cbae7a31223
Will Davis <W.Davis@mdx.ac.uk>
2020-01-16 13:45:49 +0000

    changes, no idea what. It's been a while

--- PLC_TCP_Server_Data.py
+++ PLC_TCP_Server_Data.py
@@ -3,7 +3,7 @@ import socket, sys, time
 socket.setdefaulttimeout(30000)
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Then bind() is used to associate the socket with the server address. In this case, the address is localhost, referring to the current server, and the port number is 10000.
-address = '10.3.121.133'
+address = '10.3.201.147'
 port = 9999
 # Bind the socket to the port
 server_address = (address, port)
@@ -11,9 +11,13 @@ print (sys.stderr, 'starting up on %s port %s' % server_address)
 
 sock.bind(server_address)
 # Calling listen() puts the socket into server mode, and accept() waits for an incoming connection.
-
+sending = ["0","1","2"]
+counter = 0
+c = 0
 # Listen for incoming connections
 sock.listen(1)
+
+done = False
 
 while True:
     try:
@@ -23,22 +27,22 @@ while True:
         # accept() returns an open connection between the server and client, along with the address of the client. The connection is actually a different socket on another port (assigned by the kernel). Data is read from the connection with recv() and transmitted with sendall().
 
         print (sys.stderr, 'connection from', client_address)
-
+        old = round(time.time())
+        oldList = list(str(old))
+        mod = oldList[(len(oldList)-1):]
+        mod = int(mod[0])
+        print("mod", mod)
         # Receive the data in small chunks and retransmit it
         while True:
             try:
-                connection.sendall("hello2".encode())
-                time.sleep(1)
-                # data = connection.recv(10)
-                # if data:
-                #     print(data)
-                #     data = ''
-                #
-                #     # time.sleep(1)
-                # else:
-                #     print (sys.stderr, 'no more data from', client_address)
-                #     dataFollowing = 0
-                #     break
+                var = round(time.time())
+                if var % mod == 0 and done == True:
+                    print("sending", sending[c])
+                    connection.sendall(sending[c].encode())
+                    c += 1
+                if var % mod != 0:
+                    done = False
+                    connection.sendall("".encode())
             except KeyboardInterrupt:
                 break
 
