0b76dd037f61d56db909d28639fee22cdfde1f6a
Will <Will@pl-00110355.uni.mdx.ac.uk>
2020-05-18 17:43:31 +0100

    PATH FOLLOWING

---
+++ Assets/Code/paths and stuff/follower.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PathCreation;
+
+public class follower : MonoBehaviour
+{
+    public PathCreator pathCreator;
+    public float speed = 5;
+    float distanceTravelled;
+
+    void Update()
+    {
+        distanceTravelled += speed * Time.deltaTime;
+        transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled);
+        transform.rotation = pathCreator.path.GetRotationAtDistance(distanceTravelled);
+        transform.rotation = Quaternion.Euler(0f, transform.eulerAngles.y + 270, 0f);
+
+        Debug.Log(pathCreator.path.GetRotationAtDistance(distanceTravelled));
+    }
+}
