2797b341d5bb1f3bfdbfae470a8749818e666a61
Will <Will@pl-00110355.uni.mdx.ac.uk>
2020-05-18 22:27:45 +0100

    Added spawn locations for the first island

--- Assets/Code/Master Controller/runInSimMode.cs
+++ Assets/Code/Master Controller/runInSimMode.cs
@@ -9,25 +9,53 @@ public class runInSimMode : MonoBehaviour
     public int caseSwitch = 0;
 
     public bool manualStartInduction = false;
-    public bool manualStopInduction = false;
+    public bool manualStopInduction = false; //SPAWN LOCATION 2 = follower: 92%.
     public bool manualEndInduction = false;
     public bool camInspectStartInduction = false;
-    public bool camInspectStopInduction = false;
+    public bool camInspectStopInduction = false; //SPAWN LOCATION 3 = follower: 17%.
     public bool camInspectEndInduction = false;
-    public bool codesys1StopInduction = false;
+    public bool codesys1StopInduction = false; //SPAWN LOCATION 4 = follower: 36.5%.
     public bool magFrontStartInduction = false;
-    public bool magFrontStopInduction = false;
+    public bool magFrontStopInduction = false; //SPAWN LOCATION 1 = follower: 67%.
     public bool magFrontEndInduction = false;
+
+    public GameObject carrierPrefab;
+    public GameObject[] carriers;
+    int[] carrierArray = { 0, 0, 0, 0, 0 };
+
+
+    public int ID; //delete this after.
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        carriers = new GameObject[5];
+
+
+        //if (carrierArray[manual["ID"]] == 0 && manual["rel"] == 1)
+        //{
+        //int ID = manual["ID"];
+        ID = 2; //just for testing purposes.
+        Debug.Log(carrierArray[ID]);
+        Debug.Log("creating a carrier");
+        GameObject clone = Instantiate(carrierPrefab) as GameObject;
+        carriers[ID] = clone;
+        carriers[ID].GetComponent<follower>().carrierID = ID;
+        carriers[ID].GetComponent<follower>().spawnLocation = 2;
+        carrierArray[ID] = ID;
+        //carriers[ID].SetActive(false);
+        //}
     }
 
     // Update is called once per frame
     void Update()
     {
+     
+       carriers[ID].GetComponent<follower>().percentLap += 0.25f;
+       
+
+   
+        
         Prog();
     }
 
--- Assets/Code/paths and stuff/follower.cs
+++ Assets/Code/paths and stuff/follower.cs
@@ -6,16 +6,28 @@ using PathCreation;
 public class follower : MonoBehaviour
 {
     public PathCreator firstIsland;
-    public float speed = 5;
+    public float speed = 0;
     public float distanceTravelled;
     public float percentLap;
 
+    public int carrierID;
+    public int spawnLocation;
+
+    private void Start()
+    {
+        if (spawnLocation == 1) { percentLap = 67; }
+        if (spawnLocation == 2) { percentLap = 92; }
+        if (spawnLocation == 3) { percentLap = 17; }
+        if (spawnLocation == 4) { percentLap = 36.5f; }
+    }
+
     private void Update()
     {
-        //3.753859
-        //distanceTravelled = (percentLap / 100) * 3.753859f;
 
-        distanceTravelled -= speed * Time.deltaTime;
+        //3.753859
+        distanceTravelled = (percentLap / 100) * -3.753859f;
+
+        //distanceTravelled -= speed * Time.deltaTime;
 
         transform.position = firstIsland.path.GetPointAtDistance(distanceTravelled);
         transform.rotation = firstIsland.path.GetRotationAtDistance(distanceTravelled);
